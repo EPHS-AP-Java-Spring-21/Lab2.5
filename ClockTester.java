4dae23e09b0f4788d630f9cebeb569e3f48f4486
MrMayRRHS <32204762+MrMayRRHS@users.noreply.github.com>
2020-09-29 19:15:39 -0500

    Add files via upload

---
+++ ClockTester.java
@@ -0,0 +1,15 @@
+public class ClockTester
+{
+  public static void main(String[] args)
+  {
+    Clock c = new Clock(3, 30);
+    
+    System.out.println( c );
+    
+    //c.tick(15);
+    //System.out.println( c );
+    
+    //c.tick(35);
+    //System.out.println( c );
+  }
+}
